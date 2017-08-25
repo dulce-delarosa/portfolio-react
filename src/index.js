c028c2274bba54e63e1a830a153622147a20cc93
Dulce De La Rosa <dulce@hitchhq.com>
2017-08-25 13:39:09 +0200

    BrowserRouter replaced by HashRouter

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom';
 import Home from './js/pages/Home';
 import About from './js/pages/About';
 import Contact from './js/pages/Contact';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {HashRouter as HashRouter, Route, Link} from 'react-router-dom'
 
 const Routes = () => (
-  <Router>
+  <HashRouter>
   	<div>
 	    <Route exact path="/" component={Home}/>
 	    <Route path="/about" component={About}/>
 	    <Route path="/contact" component={Contact}/>
 	    <footer>Dulce De La Rosa © Copyright 2017</footer>
 	   </div>
-  </Router>
+  </HashRouter>
 )
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
