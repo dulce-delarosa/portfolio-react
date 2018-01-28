f3037d95a044c4dec7f7ca83cdaf9ebc80686cde
Dulce De La Rosa <delarosadesign@gmail.com>
2018-01-28 19:07:25 +0100

    Quick home links fix

--- src/js/components/home/Devex.js
+++ src/js/components/home/Devex.js
@@ -7,17 +7,19 @@ class Devex extends Component {
   render() {
     return (
 			<div className="home__devex">
-				<span className="diagonal home__devex__diagonal" />
-				<div className="home__company home__devex__company">
-					<a className="home__company__mask" target="_blank" title="Devex detail page" href="https://www.devex.com/">
-						<div className="home__company__wrap">
-							<CompanyRole>Product designer &amp; Frontend dev.</CompanyRole>
-							<CompanyName>Dev<span>ex</span></CompanyName>
-							<CompanyDescription>Platform for the global<br/>development<br/>community</CompanyDescription>
-						</div>
-					</a>
-				</div>
-				<img className="home__devex__img" alt="Devex homepage screenshot" src={"images/home-devex.png"} />
+        <a target="_blank" title="Devex detail page" href="https://www.devex.com/">
+  				<span className="diagonal home__devex__diagonal" />
+  				<div className="home__company home__devex__company">
+  					<a className="home__company__mask">
+  						<div className="home__company__wrap">
+  							<CompanyRole>Product designer &amp; Frontend dev.</CompanyRole>
+  							<CompanyName>Dev<span>ex</span></CompanyName>
+  							<CompanyDescription>Platform for the global<br/>development<br/>community</CompanyDescription>
+  						</div>
+  					</a>
+  				</div>
+  				<img className="home__devex__img" alt="Devex homepage screenshot" src={"images/home-devex.png"} />
+        </a>
 			</div>
     );
   }
--- src/js/components/home/Hitch.js
+++ src/js/components/home/Hitch.js
@@ -7,17 +7,19 @@ class Hitch extends Component {
   render() {
     return (
 			<div className="home__hitch group">
-				<span className="diagonal" />
-				<img className="home__hitch__img" alt="HitchHQ homepage screenshot" src={"images/home-hitch.png"} />
-				<div className="home__company home__hitch__company">
-					<a className="home__company__mask" title="HitchHQ detail page" href="#/hitch">
-						<div className="home__company__wrap">
-							<CompanyRole>Lead designer  &amp; Frontend dev.</CompanyRole>
-							<CompanyName>Hit<span>ch</span></CompanyName>
-							<CompanyDescription>API platform</CompanyDescription>
-						</div>
-					</a>
-				</div>
+        <a href="#/hitch" title="HitchHQ detail page">
+    			<span className="diagonal" />
+    			<img className="home__hitch__img" alt="HitchHQ homepage screenshot" src={"images/home-hitch.png"} />
+    			<div className="home__company home__hitch__company">
+    				<a className="home__company__mask">
+    					<div className="home__company__wrap">
+    						<CompanyRole>Lead designer  &amp; Frontend dev.</CompanyRole>
+    						<CompanyName>Hit<span>ch</span></CompanyName>
+    						<CompanyDescription>API platform</CompanyDescription>
+    					</div>
+    				</a>
+    			</div>
+        </a>
 			</div>
     );
   }
