1bf0f58032446c723475ddb88376e7dbc2555496
dulce-delarosa <delarosadesign@gmail.com>
2018-10-21 19:19:01 +0200

    Updates

---
+++ precache-manifest.6294a64701b6cb9ade4ff0ce83f31dea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1d7c6e085a31e9f5e4745c9aef6eb4b",
+    "url": "/static/media/Avenir-Roman.b1d7c6e0.otf"
+  },
+  {
+    "revision": "3a678e95270819252977a6d9f03c91d2",
+    "url": "/static/media/Avenir-Roman.3a678e95.ttf"
+  },
+  {
+    "revision": "ea6bc2fc6af4fbc685d9adf404d40144",
+    "url": "/static/media/Avenir-Black.ea6bc2fc.ttf"
+  },
+  {
+    "revision": "b1abb878e2529cb5cb4450139844155d",
+    "url": "/static/media/Avenir-Black.b1abb878.otf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a5b3dabb581a28f44b65",
+    "url": "/static/js/main.a5b3dabb.chunk.js"
+  },
+  {
+    "revision": "2d57825e9e49b2307c7a",
+    "url": "/static/js/1.2d57825e.chunk.js"
+  },
+  {
+    "revision": "a5b3dabb581a28f44b65",
+    "url": "/static/css/main.ff7d1114.chunk.css"
+  },
+  {
+    "revision": "a9638b0c873c827054cef5e2df551fc5",
+    "url": "/index.html"
+  }
+];
